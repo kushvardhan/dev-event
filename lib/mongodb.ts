322313789183318e4bd93effecdc3da92c97aa24
Kush Vardhan <kushvardhan39797@gmail.com>
2025-11-17 19:22:18 +0530

    mongoDB CONNECTION

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,44 +1,24 @@
-/* eslint-disable prefer-const */
 import mongoose from "mongoose";
 
-type MongooseCache = {
-    conn: typeof mongoose | null;
-    promise: Promise<typeof mongoose> | null;
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) return;
+
+  if (!process.env.MONGODB_URI) {
+    throw new Error("MONGODB_URI not found in .env file");
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI);
+
+    isConnected = db.connection.readyState === 1;
+
+    console.log("MongoDB Connected Successfully");
+  } catch (err) {
+    console.log("MongoDB Connection Error => ", err);
+    throw err;
+  }
 };
 
-declare global {
-    var mongooseCache: MongooseCache | undefined;
-}
-
-const MONGODB_URI = process.env.MONGO_URL;
-
-if (!MONGODB_URI) {
-    throw new Error("Please define the MONGO_URL environment variable inside .env.local");
-}
-
-let cached: MongooseCache = global.mongooseCache || { conn: null, promise: null };
-
-if (!global.mongooseCache) {
-    global.mongooseCache = cached;
-}
-
-export async function connectDB(): Promise<typeof mongoose> {
-    if (cached.conn) return cached.conn;
-
-    if (!cached.promise) {
-        const opts = { bufferCommands: false };
-
-        cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongooseInstance) => {
-            return mongooseInstance;
-        });
-    }
-
-    try {
-        cached.conn = await cached.promise;
-    } catch (error) {
-        cached.promise = null;
-        throw error;
-    }
-
-    return cached.conn;
-}
+export default connectDB;
