98a55f925654679426736339bfb88efbaaac8125
Kush Vardhan <kushvardhan39797@gmail.com>
2025-11-18 17:43:13 +0530

    Event Action

---
+++ lib/actions/event.actions.ts
@@ -0,0 +1,24 @@
+'use server';
+
+import Event, { IEvent } from '@/database/event.model';
+import connectDB from "@/lib/mongodb";
+
+export const getSimilarEventsBySlug = async (slug: string): Promise<IEvent[]> => {
+  try {
+    await connectDB();
+
+    const event = await Event.findOne({ slug }).lean<IEvent>();
+
+    if (!event) return [];
+
+    const similarEvents = await Event.find({
+      _id: { $ne: event._id },
+      tags: { $in: event.tags }
+    }).lean<IEvent[]>();
+
+    return similarEvents;
+  } catch (error) {
+    console.error("Similar events error:", error);
+    return [];
+  }
+};
