3b9ca07b735530f6e0996e44fc155e38ebd42f97
Kush Vardhan <kushvardhan39797@gmail.com>
2025-11-18 18:18:10 +0530

    installed and setup the POSTHOG

--- components/BookEvent.tsx
+++ components/BookEvent.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import {useState} from "react";
-import { createBooking } from "@/lib/actions/booking.actions";
+import {createBooking} from "@/lib/actions/booking.actions";
+import posthog from "posthog-js";
 
 const BookEvent = ({ eventId, slug }: { eventId: string, slug: string;}) => {
     const [email, setEmail] = useState('');
@@ -14,8 +15,10 @@ const BookEvent = ({ eventId, slug }: { eventId: string, slug: string;}) => {
 
         if(success) {
             setSubmitted(true);
+            posthog.capture('event_booked', { eventId, slug, email })
         } else {
             console.error('Booking creation failed')
+            posthog.captureException('Booking creation failed')
         }
     }
 
