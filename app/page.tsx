cb886c1943f13b0e74d82ce26ae5a23bb462d4e9
Kush Vardhan <kushvardhan39797@gmail.com>
2025-11-19 18:29:01 +0530

    refactor: clean up unused event data code

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { IEvent } from "@/database";
 import { getAllEvents } from "@/lib/actions/event.actions";
 // import { events } from "@/lib/constants";
 
-
 // const events=[
 //   {id:1, image:'/images/event1.png', title:'Event 1', slug:'event-1', location:'location-1',date:'date-1', time:'Time-1'},
 //   {id:2, image:'/images/event2.png', title:'Event 2', slug:'event-2', location:'location-2',date:'date-2', time:'Time-2'},
@@ -15,29 +14,33 @@ import { getAllEvents } from "@/lib/actions/event.actions";
 
 // ]
 
-
 export default async function Home() {
-    const events = await getAllEvents();
-
+  const events = await getAllEvents();
 
   return (
     <section>
-      <h1 className="text-center">The Hub of Every Day <br /> Event You Can&apos;t Miss</h1>
-      <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place.</p>
-      
-      <ExploreBtn/>
+      <h1 className="text-center">
+        The Hub of Every Day <br /> Event You Can&apos;t Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place.
+      </p>
 
-      <div className='mt-20 space-y-7'>
-          <h3>Featured Events</h3>
+      <ExploreBtn />
 
-          <ul className="events">
-            {events && events.length > 0 && events.map((events:IEvent,index)=>(
-              <li key={index}><EventCard {...events} /></li>
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li className="list-none" key={event.slug}>
+                <EventCard {...event} />
+              </li>
             ))}
-          </ul>
-
+        </ul>
       </div>
-
     </section>
   );
 }
