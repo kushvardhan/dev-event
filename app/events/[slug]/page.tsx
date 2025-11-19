1530cb6f4069b9bf201f4c795037303275290382
Kush Vardhan <kushvardhan39797@gmail.com>
2025-11-19 19:01:08 +0530

    Suspense for loading

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,15 +1,20 @@
-import {Suspense} from "react";
+import { Suspense } from "react";
 import EventDetails from "@/components/EventDetails";
 
-const EventDetailsPage = async ({ params }: { params: { slug: string }}) => {
-    const { slug } = params;
+const EventDetailsPage = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { slug } = await params;
 
-    return (
-        <main>
-            <Suspense fallback={<div>Loading...</div>}>
-                <EventDetails slug={slug} />
-            </Suspense>
-        </main>
-    )
-}
-export default EventDetailsPage
+  return (
+    <main>
+      <Suspense fallback={<div>Loading...</div>}>
+        <EventDetails slug={slug} />
+      </Suspense>
+    </main>
+  );
+};
+
+export default EventDetailsPage;
